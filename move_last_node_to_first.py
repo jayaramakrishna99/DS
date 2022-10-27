12aa57b1d754b8f3f2df382ce14aba6d7e7d8c63
jayaramakrishna99 <102653712+jayaramakrishna99@users.noreply.github.com>
2022-10-27 20:48:12 +0530

    Add files via upload

---
+++ move_last_node_to_first.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class SLL:
+    def __init__(self):
+        self.head = None
+        self.last_node = None
+
+    def print(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=' ')
+            temp = temp.next
+
+    def append(self, data):
+        if self.last_node is None:
+            self.head = Node(data)
+            self.last_node = self.head
+        else:
+            self.last_node.next = Node(data)
+            self.last_node = self.last_node.next
+
+    def move_lto_f(self):
+        temp = self.head
+        while temp.next.next:
+            temp = temp.next
+        temp.next.next = self.head
+        self.head = temp.next
+        temp.next = None
+l = SLL()
+n = int(input('n no--->'))
+for i in range(n):
+    data = int(input('data-->'))
+    l.append(data)
+l.move_lto_f()
+l.print()
