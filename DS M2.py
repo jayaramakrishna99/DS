8d9e4b139962beca1be54c4a82f7299d4ffaa28f
jayaramakrishna99 <102653712+jayaramakrishna99@users.noreply.github.com>
2022-12-07 16:40:44 +0530

    Update DS M2.py

--- DS M2.py
+++ DS M2.py
@@ -3,7 +3,7 @@ class Node:
         self.left=None
         self.data=data
         self.right=None
-
+# Inserting values in BST Tree
 def bst_ins(root,data):
     if root is None:
         return Node(data)
@@ -13,15 +13,7 @@ def bst_ins(root,data):
         root.left=bst_ins(root.left,data)
     return root
 
-def check_bal(root):
-    if root is None:
-        return
-    check_bal(root.left)
-    if -1<=balfac(root)<=1:
-        return True
-    check_bal(root.right)
-    return False
-
+# Level order Traversal
 def levelorder(root):
     q=[]
     q.append(root)
@@ -33,34 +25,45 @@ def levelorder(root):
         if top.right!=None:
             q.append(top.right)
 
-
+# A. Maximum Element in the BST
 def maxi(root):
     while root.right!=None:
         root=root.right
     return root.data
 
+# Height of the Tree
 def height(root):
     if root is None:
         return 0
     return max(height(root.left),height(root.right))+1
-
+# Balance factor of root
 def balfac(root):
     return height(root.left)-height(root.right)
 
+# B. Checking Balancing factor of root in BST
+def check_bal(root):
+    if root is None:
+        return
+    check_bal(root.left)
+    if -1<=balfac(root)<=1:
+        return True
+    check_bal(root.right)
+    return False
+# Right rotation
 def rightrotate(root):
     temp=root.left
     t=temp.right
     temp.right=root
     root.left=t
     return temp
-
+# Left rotation
 def leftrotate(root):
     temp=root.right
     t=temp.left
     temp.left=root
     root.right=t
     return temp
-
+# Inserting values in AVl Tree
 def avl_ins(root,data):
     if root is None:
         return Node(data)
@@ -82,13 +85,19 @@ def avl_ins(root,data):
             root.right=rightrotate(root.right)
             return leftrotate(root)
     return root
-
+# l=[100,70,50,40,60,75,80]
 root=None
-# l=[int(i) for i in input("Enter the list:").split()]
-l=[100,60,50,70,30,55,65,75,120,125]
+l=[int(i) for i in input().split()]
 for i in l:
-    root=avl_ins(root,i)
+    root=bst_ins(root,i)
+print("maximum element:",maxi(root))
 levelorder(root)
-# print(maxi(root))
-# print(mini(root))
-# print(check_bal(root))
+if check_bal(root)==True:
+    print("BST is balanced and It is AVL.")
+else:
+    print("BST is Not Balanced and Balanced is")
+    root1=None
+    for i in l:
+        root1=avl_ins(root1,i)
+    levelorder(root1)
+
